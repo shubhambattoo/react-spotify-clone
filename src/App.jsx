d2f5f627a626866d8645593633a6fa56755bfae8
shubhambattoo <shubham.battoo@hotmail.com>
2020-08-24 00:08:43 +0530

    added context

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,39 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromResponse } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState('');
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromResponse();
     window.location.hash = '';
     const _token = hash.access_token;
     if (_token) {
-      setToken(_token);
+      dispatch({type: 'SET_TOKEN', payload: _token})
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((account) => {
+        dispatch({ type: 'SET_USER', payload: account });
+      });
     }
-  }, []);
+  }, [dispatch]);
 
-  return <div>{token ? <>My App</> : <Login />}</div>;
+  return (
+    <div>
+      {token ? (
+        <>
+          <Player />
+        </>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/reducer.js
@@ -0,0 +1,21 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
+  playing: false,
+  item: null,
+  token: null,
+};
+
+export default function (state, action) {
+  switch (action.type) {
+    case 'SET_USER':
+      return { ...state, user: action.payload };
+    case 'SET_TOKEN':
+      return { ...state, token: action.payload };
+    default:
+      return state;
+  }
+}
